cdde2134ddfc04c70f4c8431ed9b1326a91cfbfe
crayonzgrim <crayonzgrim@gmail.com>
2023-09-10 17:03:35 +0900

    :hammer: Header layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import { Header } from '@/components';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Book Search App",
-  description: "Search books with operator",
+  title: 'Book Search App',
+  description: 'Search books with operator'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/layout/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const Header = () => {
+  return (
+    <div className="flex justify-end items-center mx-2 max-w-6xl sm:mx-auto py-6">
+      <div className="flex items-center ml-3">
+        <Link href="/">
+          <h2 className="flex items-center text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-4 py-2 rounded-lg mr-1">
+              BOOK SEARCH APP
+            </span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
